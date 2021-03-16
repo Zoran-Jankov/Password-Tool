d538dbb101859375d7bb1f953106dac5a9eea9cf
Zoran Jankov <zoran.jankov.87@gmail.com>
2021-03-16 13:49:52 +0100

    implementation of only locked out users filter and code refactoring

--- Password-Tool.ps1
+++ Password-Tool.ps1
@@ -3,6 +3,7 @@
 $TempPassword = "Privremen0"
 $SearchBase = "OU=Korisnici,OU=Centrala,DC=uni,DC=net"
 $Admin = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
+$Users = $null
 
 #-----------------------------------------------------------[Functions]------------------------------------------------------------
 
@@ -140,44 +141,121 @@ function Get-AccountsInfo {
         $SearchBase
     )
 
-    begin {
-        $DomainSuffix = "@" + (Get-ADDomain).Forest
-    }
-
     process {
-        $Users = Get-ADUser -SearchBase $SearchBase -Filter {Enabled -eq $true} `
-            -Properties 'LockedOut', 'PasswordLastSet' | Sort-Object -Property 'UserPrincipalName'
+        $Accounts = Get-ADUser -SearchBase $SearchBase -Filter {Enabled -eq $true} `
+            -Properties 'LockedOut', 'PasswordLastSet', 'UserPrincipalName' | Sort-Object -Property 'UserPrincipalName'
 
-        $UsersTable = [ordered]@{}
-        foreach ($Accout in $Users) {
-            $UsersTable.Add(($Accout.UserPrincipalName).Replace($DomainSuffix, ""), $Accout)
-        }
-        return $UsersTable
+        return $Accounts
     }
 }
 
-function Update-Display {
-    $SelectedUser = $UsersTable.Get_Item($UserComboBox.Text)
-    if ($null -ne $SelectedUser) {
-        if ($SelectedUser.LockedOut) {
-            $BlockedLabel.Text = "Locked Out"
-            $BlockedLabel.ForeColor = [System.Drawing.ColorTranslator]::FromHtml("#ff0000")
+<#
+.SYNOPSIS
+Short description
+
+.DESCRIPTION
+Long description
+
+.PARAMETER OnlyLockedOut
+Parameter description
+
+.PARAMETER Users
+Parameter description
+
+.EXAMPLE
+An example
+
+.NOTES
+Version:        1.0
+Author:         Zoran Jankov
+#>
+function Get-ComboBoxItems {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true,
+                   Position = 0,
+                   ValueFromPipeline = $true,
+                   ValueFromPipelineByPropertyName = $true,
+                   HelpMessage = "Specifies if only locked out are to be added")]
+        [bool]
+        $OnlyLockedOut,
+
+        [Parameter(Mandatory = $true,
+                   Position = 1,
+                   ValueFromPipeline = $true,
+                   ValueFromPipelineByPropertyName = $true,
+                   HelpMessage = "AD user list")]
+        [Object[]]
+        $Accounts
+    )
+
+    process {
+        $ComboBoxItems = @()
+        foreach ($Account in $Accounts) {
+            if ($OnlyLockedOut) {
+                if ($Account.LockedOut) {
+                    $ComboBoxItems += $Account.UserPrincipalName
+                }
+            }
+            else {
+                $ComboBoxItems += $Account.UserPrincipalName
+            }
+        }
+        return $ComboBoxItems
+    }
+}
+
+function Get-DisplayData {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true,
+                   Position = 0,
+                   ValueFromPipeline = $true,
+                   ValueFromPipelineByPropertyName = $true,
+                   HelpMessage = "")]
+        [String]
+        $Username,
+
+        [Parameter(Mandatory = $true,
+                   Position = 1,
+                   ValueFromPipeline = $true,
+                   ValueFromPipelineByPropertyName = $true,
+                   HelpMessage = "")]
+        $UserBase
+    )
+
+    process {
+        $SelectedUser = $UserBase | Where-Object {$_.UserPrincipalName -eq $Username}
+        if ($null -ne $SelectedUser) {
+            if ($SelectedUser.LockedOut) {
+                $BlockedLabel.Text = "Locked Out"
+                $BlockedLabel.ForeColor = [System.Drawing.ColorTranslator]::FromHtml("#ff0000")
+            }
+            else {
+                $BlockedLabel.Text = "Active"
+                $BlockedLabel.ForeColor = [System.Drawing.ColorTranslator]::FromHtml("#00ff00")
+            }
+            try {
+                $LastPassChange = $SelectedUser.PasswordLastSet.toString("dd.MM.yyyy. HH:mm")
+            }
+            catch {
+                $LastPassChange = "Unknown"
+            }
         }
         else {
-            $BlockedLabel.Text = "Active"
-            $BlockedLabel.ForeColor = [System.Drawing.ColorTranslator]::FromHtml("#00ff00")
+            $BlockedValue = ""
+            $LastPassChange = ""
         }
-        $LastPassChangeValue.Text = ($SelectedUser.PasswordLastSet.tostring("dd.MM.yyyy. HH:mm"))
-    }
-    else {
-        $BlockedLabel.Text = ""
-        $LastPassChangeValue.Text = ""
-    }
+        return @{
+            Status = $BlockedValue;
+            LastPassChange = $LastPassChange
+        }
+    }  
 }
 
 #-----------------------------------------------------------[Execution]------------------------------------------------------------
 
-$UsersTable = Get-AccountsInfo -SearchBase $SearchBase
+$Users = Get-AccountsInfo -SearchBase $SearchBase
 Add-Type -AssemblyName System.Windows.Forms
 [System.Windows.Forms.Application]::EnableVisualStyles()
 
@@ -187,6 +265,7 @@ $MainForm.text                   = "Password Tool"
 $MainForm.TopMost                = $true
 $MainForm.FormBorderStyle        = 'Fixed3D'
 $MainForm.MaximizeBox            = $false
+$MainForm.ShowIcon               = $false
 
 $UserLabel                       = New-Object system.Windows.Forms.Label
 $UserLabel.text                  = "User"
@@ -204,11 +283,17 @@ $UserComboBox.Font               = New-Object System.Drawing.Font('Microsoft San
 $UserComboBox.AutoCompleteMode   = 'SuggestAppend'
 $UserComboBox.AutoCompleteSource = 'ListItems'
 
+$OnlyLockedOutCheckbox           = New-Object System.Windows.Forms.CheckBox
+$OnlyLockedOutCheckbox.AutoSize  = $true
+$OnlyLockedOutCheckbox.Location  = New-Object System.Drawing.Size(30,90)
+$OnlyLockedOutCheckbox.Text      = "Show Only Locked Out"
+$OnlyLockedOutCheckbox.Checked   = $false
+
 $BlockedLabel                    = New-Object system.Windows.Forms.Label
 $BlockedLabel.AutoSize           = $true
 $BlockedLabel.width              = 25
 $BlockedLabel.height             = 10
-$BlockedLabel.location           = New-Object System.Drawing.Point(30,90)
+$BlockedLabel.location           = New-Object System.Drawing.Point(80,130)
 $BlockedLabel.Font               = New-Object System.Drawing.Font('Microsoft Sans Serif',10,[System.Drawing.FontStyle]([System.Drawing.FontStyle]::Bold))
 
 $UnblockButton                   = New-Object system.Windows.Forms.Button
@@ -218,20 +303,6 @@ $UnblockButton.height            = 30
 $UnblockButton.location          = New-Object System.Drawing.Point(30,180)
 $UnblockButton.Font              = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
 
-$ResetPasswordButton             = New-Object system.Windows.Forms.Button
-$ResetPasswordButton.text        = "Reset Password"
-$ResetPasswordButton.width       = 150
-$ResetPasswordButton.height      = 30
-$ResetPasswordButton.location    = New-Object System.Drawing.Point(380,180)
-$ResetPasswordButton.Font        = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
-
-$LastPassChangeValue             = New-Object system.Windows.Forms.Label
-$LastPassChangeValue.AutoSize    = $true
-$LastPassChangeValue.width       = 25
-$LastPassChangeValue.height      = 10
-$LastPassChangeValue.location    = New-Object System.Drawing.Point(390,130)
-$LastPassChangeValue.Font        = New-Object System.Drawing.Font('Microsoft Sans Serif',10,[System.Drawing.FontStyle]([System.Drawing.FontStyle]::Bold))
-
 $LastPassChangeLabel             = New-Object system.Windows.Forms.Label
 $LastPassChangeLabel.text        = "Last Password Change"
 $LastPassChangeLabel.AutoSize    = $true
@@ -240,27 +311,49 @@ $LastPassChangeLabel.height      = 10
 $LastPassChangeLabel.location    = New-Object System.Drawing.Point(390,90)
 $LastPassChangeLabel.Font        = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
 
+$LastPassChangeValue             = New-Object system.Windows.Forms.Label
+$LastPassChangeValue.AutoSize    = $true
+$LastPassChangeValue.width       = 25
+$LastPassChangeValue.height      = 10
+$LastPassChangeValue.location    = New-Object System.Drawing.Point(390,130)
+$LastPassChangeValue.Font        = New-Object System.Drawing.Font('Microsoft Sans Serif',10,[System.Drawing.FontStyle]([System.Drawing.FontStyle]::Bold))
+
+$ResetPasswordButton             = New-Object system.Windows.Forms.Button
+$ResetPasswordButton.text        = "Reset Password"
+$ResetPasswordButton.width       = 150
+$ResetPasswordButton.height      = 30
+$ResetPasswordButton.location    = New-Object System.Drawing.Point(380,180)
+$ResetPasswordButton.Font        = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
+
 $MainForm.controls.AddRange(@(
     $UserLabel,
     $UserComboBox,
+    $OnlyLockedOutCheckbox,
     $BlockedLabel,
     $UnblockButton,
-    $ResetPasswordButton,
+    $LastPassChangeLabel,
     $LastPassChangeValue,
-    $LastPassChangeLabel
-    ))
+    $ResetPasswordButton
+))
 
-foreach ($Username in $UsersTable.Keys.GetEnumerator()) {
-    $UserComboBox.Items.Add($Username)
-}
+$UserComboBox.Items.Clear()
+$UserComboBox.Items.AddRange((Get-ComboBoxItems -OnlyLockedOut $OnlyLockedOutCheckbox.Checked -Accounts $Users))
 
 $UserComboBox.Add_TextChanged({
-    Update-Display
+    $DisplayData = Get-DisplayData -Username ($UserComboBox.Text) -UserBase $Users
+    $BlockedLabel.Text = $DisplayData.Status
+    $LastPassChangeValue.Text = $DisplayData.LastPassChange
+})
+
+$OnlyLockedOutCheckbox.Add_CheckStateChanged({
+    $UserComboBox.Items.Clear()
+    $UserComboBox.Items.AddRange((Get-ComboBoxItems -OnlyLockedOut $OnlyLockedOutCheckbox.Checked -Accounts $Users))
 })
 
 $UnblockButton.Add_Click({
-    $SelectedUser = $UsersTable.Get_Item($UserComboBox.Text)
-    $UserName = $UserComboBox.Text
+    $Username = $UserComboBox.Text
+    $SelectedUser = $Users | Where-Object {$_.UserPrincipalName -eq $Username}
+    
     $Choice = [System.Windows.Forms.MessageBox]::Show("Are you sure you want to unblock $UserName user account?", "Unblock Account", 4)
     if ($Choice -eq 'Yes') {
         try {
@@ -271,15 +364,16 @@ $UnblockButton.Add_Click({
             [System.Windows.Forms.MessageBox]::Show("Failed to unblock $UserName user account", "Error")
             continue
         }
-        $UsersTable = Get-AccountsInfo -SearchBase $SearchBase
+        $Users = Get-AccountsInfo -SearchBase $SearchBase
         Update-Display
+        Update-ComboBox
         Write-Log -Message "Admin '$Admin' successfully unblocked '$UserName' user account"
         [System.Windows.Forms.MessageBox]::Show("$UserName user account successfully unblocked", "Info")
     }
 })
 
 $ResetPasswordButton.Add_Click({
-    $SelectedUser = $UsersTable.Get_Item($UserComboBox.Text)
+    $SelectedUser = $Users | Where-Object {$_.UserPrincipalName -eq $UserComboBox.Text}
     $UserName = $UserComboBox.Text
     $Choice = [System.Windows.Forms.MessageBox]::Show("Are you sure you want to reset $UserName user account password?", "Reset Account Password", 4)
     if ($Choice -eq 'Yes') {
@@ -294,12 +388,12 @@ $ResetPasswordButton.Add_Click({
             [System.Windows.Forms.MessageBox]::Show("Failed to change $UserName user account password", "Error")
             continue
         }
-        $UsersTable = Get-AccountsInfo -SearchBase $SearchBase
+        $Users = Get-AccountsInfo -SearchBase $SearchBase
         Update-Display
+        Update-ComboBox
         Write-Log -Message "Admin '$Admin' successfully changed '$UserName' user account password"
         [System.Windows.Forms.MessageBox]::Show("$UserName user account password successfully changed", "Info")
     }
-    
 })
 
 [void]$MainForm.ShowDialog()
